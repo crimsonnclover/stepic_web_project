6e1e185e4a1eda0badebb277d60d64bc2414f20c
Aleksey Bozhedomov <aleksey.bozhedomov.5@gmail.com>
2023-03-23 18:13:01 +0300

    Task 2 attempt 4

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-udo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello:application
+sudo gunicorn -b 0.0.0.0:8080 hello:application
