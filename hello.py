013f182d23069992c60c01b6af560c6268702ff9
Aleksey Bozhedomov <aleksey.bozhedomov.5@gmail.com>
2023-03-23 18:11:48 +0300

    Task 2 attempt 3

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-def appliction(environ, start_response):
+def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
